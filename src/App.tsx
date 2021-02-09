363a47e29a5d9a356b59bac411c485bf9e11ad1e
Adrian Sudbury <sudburya@gmail.com>
2021-02-09 09:04:14 +0000

    Added Search Box to the ToolBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 import React, { useState } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import InputBase from "@material-ui/core/InputBase";
 import MuiComponentSamples from "../src/MuiComponentSamples";
 import { makeStyles } from "@material-ui/core/styles";
 import CenterFocusStrongIcon from "@material-ui/icons/CenterFocusStrong";
+import SearchIcon from "@material-ui/icons/Search";
 import {
   AppBar,
   createMuiTheme,
+  fade,
   ThemeProvider,
   Toolbar,
   Typography,
@@ -24,6 +27,43 @@ const useStyles = makeStyles((theme) => ({
   },
   logoButton: {
     marginRight: theme.spacing(2),
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
   },
 }));
 
@@ -50,6 +90,19 @@ const App = () => {
           <Toolbar>
             <CenterFocusStrongIcon className={classes.logoButton} />
             <Typography variant="h6">Ruleau</Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ "aria-label": "search" }}
+              />
+            </div>
             <div className={classes.grow} />
             <div>
               <Settings
