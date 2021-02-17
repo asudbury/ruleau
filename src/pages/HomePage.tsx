6b84b14ea3440a9a4041d87a2627ed43a3e03bc7
Adrian Sudbury <sudburya@gmail.com>
2021-02-17 13:19:39 +0000

    Format Homepage content

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -34,16 +34,18 @@ export default function HomePage() {
               <Grid item md={5} component={Box} className={classes.image}>
                 <Box p={5}>
                   <Typography variant="h5">About Ruleau</Typography>
-                  <Typography variant="body1">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
-                  </Typography>
+                  <Box p={1}>
+                    <Typography variant="body1">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      Duis aute irure dolor in reprehenderit in voluptate velit
+                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                      sint occaecat cupidatat non proident, sunt in culpa qui
+                      officia deserunt mollit anim id est laborum.
+                    </Typography>
+                  </Box>
                 </Box>
               </Grid>
             </Hidden>
