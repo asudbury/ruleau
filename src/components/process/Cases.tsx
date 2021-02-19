dadcdf6431b18828fd2c9353d1806bf712dbe066
Adrian Sudbury <sudburya@gmail.com>
2021-02-19 16:47:45 +0000

    Updates to Cases

--- src/components/process/Cases.tsx
+++ src/components/process/Cases.tsx
@@ -1,12 +1,26 @@
+import React, { useEffect, createRef } from "react";
 import MaterialTable from "material-table";
 import TableIcons from "../TableIcons";
-import WorkIcon from '@material-ui/icons/Work';
+import WorkIcon from "@material-ui/icons/Work";
+import { Box } from "@material-ui/core";
 
 interface CasesProps {
   onCaseSelected: (caseID: string) => void;
 }
 
 export default function Cases({ onCaseSelected }: CasesProps) {
+  const tableRef = createRef();
+
+  useEffect(() => {
+    console.log("hello world!");
+
+    console.log(tableRef.current);
+
+    /// console.log(tableRef.current.columns[1])
+  }, [tableRef]);
+
+  console.log(tableRef);
+
   function handleSelectedRow(
     selectedRow:
       | {
@@ -19,7 +33,6 @@ export default function Cases({ onCaseSelected }: CasesProps) {
         }
       | undefined
   ) {
-
     if (selectedRow) {
       const caseId = selectedRow.caseID;
 
@@ -28,257 +41,278 @@ export default function Cases({ onCaseSelected }: CasesProps) {
   }
 
   return (
-    <MaterialTable
-      title=""
-      icons={TableIcons}
-      columns={[
-        { title: "Case ID", field: "caseID",  render: rowData => <div><WorkIcon fontSize="small" color="secondary" /> {rowData.caseID}</div>},
-        { title: "Execution No.", field: "executionNo" },
-        { title: "Date Processed", field: "dateProcessed" },
-        {
-          title: "Open/Close",
-          field: "status",
-          lookup: { 1: "Open", 2: "Closed" },
-        },
-        {
-          title: "Result",
-          field: "result",
-          lookup: { 1: "Passed", 2: "Warning", 3: "Failed" },
-        },
-        { title: "No. of Failures", field: "failures" },
-      ]}
-      data={[
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r192344",
-          executionNo: "1",
-          status: 1,
-          result: 3,
-          failures: 3,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r192345",
-          executionNo: "1",
-          status: 1,
-          result: 1,
-          failures: 1,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r192341",
-          executionNo: "1",
-          status: 1,
-          result: 3,
-          failures: 3,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r192342",
-          executionNo: "1",
-          status: 2,
-          result: 1,
-          failures: 1,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r192343",
-          executionNo: "1",
-          status: 1,
-          result: 3,
-          failures: 3,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r192355",
-          executionNo: "1",
-          status: 2,
-          result: 1,
-          failures: 1,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r192354",
-          executionNo: "1",
-          status: 1,
-          result: 3,
-          failures: 3,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r192365",
-          executionNo: "1",
-          status: 1,
-          result: 1,
-          failures: 1,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r192374",
-          executionNo: "1",
-          status: 1,
-          result: 3,
-          failures: 3,
-        },
-        {
-          dateProcessed: "25 July 2020",
-          caseID: "r193365",
-          executionNo: "1",
-          status: 2,
-          result: 1,
-          failures: 1,
-        },
-        {
-          dateProcessed: "26 July 2020",
-          caseID: "r192344",
-          executionNo: "1",
-          status: 1,
-          result: 3,
-          failures: 3,
-        },
-        {
-          dateProcessed: "24 August 2020",
-          caseID: "r196345",
-          executionNo: "1",
-          status: 1,
-          result: 1,
-          failures: 1,
-        },
-        {
-          dateProcessed: "24 September 2020",
-          caseID: "r792344",
-          executionNo: "1",
-          status: 1,
-          result: 3,
-          failures: 3,
-        },
-        {
-          dateProcessed: "01 October 2020",
-          caseID: "r992345",
-          executionNo: "1",
-          status: 1,
-          result: 1,
-          failures: 1,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r292344",
-          executionNo: "1",
-          status: 1,
-          result: 3,
-          failures: 2,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r392345",
-          executionNo: "1",
-          status: 1,
-          result: 1,
-          failures: 1,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r492344",
-          executionNo: "1",
-          status: 1,
-          result: 3,
-          failures: 9,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r192345",
-          executionNo: "1",
-          status: 1,
-          result: 1,
-          failures: 4,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r192344",
-          executionNo: "1",
-          status: 1,
-          result: 3,
-          failures: 3,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r792345",
-          executionNo: "1",
-          status: 1,
-          result: 1,
-          failures: 1,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r192344",
-          executionNo: "1",
-          status: 1,
-          result: 3,
-          failures: 3,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r192345",
-          executionNo: "1",
-          status: 1,
-          result: 1,
-          failures: 1,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r198344",
-          executionNo: "1",
-          status: 1,
-          result: 3,
-          failures: 3,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r192345",
-          executionNo: "1",
-          status: 1,
-          result: 1,
-          failures: 1,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r198744",
-          executionNo: "1",
-          status: 1,
-          result: 3,
-          failures: 3,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r192365",
-          executionNo: "1",
-          status: 1,
-          result: 1,
-          failures: 1,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r194544",
-          executionNo: "1",
-          status: 1,
-          result: 3,
-          failures: 3,
-        },
-        {
-          dateProcessed: "24 July 2020",
-          caseID: "r192445",
-          executionNo: "1",
-          status: 1,
-          result: 1,
-          failures: 1,
-        },
-      ]}
-      onRowClick={(evt, selectedRow) => handleSelectedRow(selectedRow)}
-      options={{
-        filtering: true,
-        exportButton: true,
-        exportFileName: "cases",
-      }}
-    />
+    <div>
+      <Box component="div"  overflow="auto">
+        <div>
+          <MaterialTable
+            tableRef={tableRef}
+            title=""
+            icons={TableIcons}
+            onFilterChange={(filters) => {
+              console.log("onFilterChange", filters);
+            }}
+            columns={[
+              {
+                title: "Case ID",
+                field: "caseID",
+                render: (rowData) => (
+                  <div>
+                    <WorkIcon fontSize="small" color="secondary" />{" "}
+                    {rowData.caseID}
+                  </div>
+                ),
+              },
+              { title: "Execution No.", field: "executionNo" },
+              { title: "Date Processed", field: "dateProcessed" },
+              {
+                title: "Open/Close",
+                field: "status",
+                lookup: { 1: "Open", 2: "Closed" },
+              },
+              {
+                title: "Result",
+                field: "result",
+                lookup: { 1: "Passed", 2: "Warning", 3: "Failed" },
+              },
+              { title: "No. of Failures", field: "failures" },
+            ]}
+            data={[
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r192344",
+                executionNo: "1",
+                status: 1,
+                result: 3,
+                failures: 3,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r192345",
+                executionNo: "1",
+                status: 1,
+                result: 1,
+                failures: 1,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r192341",
+                executionNo: "1",
+                status: 1,
+                result: 3,
+                failures: 3,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r192342",
+                executionNo: "1",
+                status: 2,
+                result: 1,
+                failures: 1,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r192343",
+                executionNo: "1",
+                status: 1,
+                result: 3,
+                failures: 3,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r192355",
+                executionNo: "1",
+                status: 2,
+                result: 1,
+                failures: 1,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r192354",
+                executionNo: "1",
+                status: 1,
+                result: 3,
+                failures: 3,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r192365",
+                executionNo: "1",
+                status: 1,
+                result: 1,
+                failures: 1,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r192374",
+                executionNo: "1",
+                status: 1,
+                result: 3,
+                failures: 3,
+              },
+              {
+                dateProcessed: "25 July 2020",
+                caseID: "r193365",
+                executionNo: "1",
+                status: 2,
+                result: 1,
+                failures: 1,
+              },
+              {
+                dateProcessed: "26 July 2020",
+                caseID: "r192344",
+                executionNo: "1",
+                status: 1,
+                result: 3,
+                failures: 3,
+              },
+              {
+                dateProcessed: "24 August 2020",
+                caseID: "r196345",
+                executionNo: "1",
+                status: 1,
+                result: 1,
+                failures: 1,
+              },
+              {
+                dateProcessed: "24 September 2020",
+                caseID: "r792344",
+                executionNo: "1",
+                status: 1,
+                result: 3,
+                failures: 3,
+              },
+              {
+                dateProcessed: "01 October 2020",
+                caseID: "r992345",
+                executionNo: "1",
+                status: 1,
+                result: 1,
+                failures: 1,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r292344",
+                executionNo: "1",
+                status: 1,
+                result: 3,
+                failures: 2,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r392345",
+                executionNo: "1",
+                status: 1,
+                result: 1,
+                failures: 1,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r492344",
+                executionNo: "1",
+                status: 1,
+                result: 3,
+                failures: 9,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r192345",
+                executionNo: "1",
+                status: 1,
+                result: 1,
+                failures: 4,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r192344",
+                executionNo: "1",
+                status: 1,
+                result: 3,
+                failures: 3,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r792345",
+                executionNo: "1",
+                status: 1,
+                result: 1,
+                failures: 1,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r192344",
+                executionNo: "1",
+                status: 1,
+                result: 3,
+                failures: 3,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r192345",
+                executionNo: "1",
+                status: 1,
+                result: 1,
+                failures: 1,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r198344",
+                executionNo: "1",
+                status: 1,
+                result: 3,
+                failures: 3,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r192345",
+                executionNo: "1",
+                status: 1,
+                result: 1,
+                failures: 1,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r198744",
+                executionNo: "1",
+                status: 1,
+                result: 3,
+                failures: 3,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r192365",
+                executionNo: "1",
+                status: 1,
+                result: 1,
+                failures: 1,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r194544",
+                executionNo: "1",
+                status: 1,
+                result: 3,
+                failures: 3,
+              },
+              {
+                dateProcessed: "24 July 2020",
+                caseID: "r192445",
+                executionNo: "1",
+                status: 1,
+                result: 1,
+                failures: 1,
+              },
+            ]}
+            onRowClick={(evt, selectedRow) => handleSelectedRow(selectedRow)}
+            options={{
+              filtering: true,
+              exportButton: true,
+              exportFileName: "cases",
+              pageSize: 10,
+              pageSizeOptions: [5, 10, 50, 100, 500, 1000],
+            }}
+          />
+        </div>
+      </Box>
+    </div>
   );
 }
--- src/pages/CasePage.tsx
+++ src/pages/CasePage.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import { Grid, Box, makeStyles, MenuItem, Select } from "@material-ui/core";
+import {
+  Grid,
+  Box,
+  makeStyles,
+  MenuItem,
+  Select,
+  Button,
+} from "@material-ui/core";
 import LabelAndValue from "../components/core/LabelAndValue";
 
 export default function CasePage() {
@@ -20,7 +27,7 @@ export default function CasePage() {
           <Grid item xs={12}>
             <LabelAndValue variant="h5" label="Case" value="Unknown" />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12} sm={6}>
             <div style={{ display: "flex", alignItems: "baseline" }}>
               <Typography variant="body1">Execution:</Typography>
               <Select value={3} style={{ marginLeft: 10 }}>
@@ -30,26 +37,36 @@ export default function CasePage() {
               </Select>
             </div>
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12} sm={6}>
             <LabelAndValue
               variant="body1"
               label="Processed On"
               value="12 July 2020"
             />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12} sm={6}>
             <LabelAndValue
               variant="body1"
               label="Name"
               value="Test Application"
             />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12} sm={6}>
             <LabelAndValue
               variant="body1"
               label="Date of Birth"
               value="03 March 1975"
             />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Button
+              data-testId="closeCase"
+              type="submit"
+              variant="outlined"
+              color="secondary"
+            >
+              Close Case
+            </Button>
           </Grid>
         </Grid>
         <Grid container spacing={5}>
