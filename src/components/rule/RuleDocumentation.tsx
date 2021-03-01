50afb4f2b2bf570fa26ac9b44906c673b24f0a43
Adrian Sudbury <sudburya@gmail.com>
2021-03-01 16:06:16 +0000

    Updated themes remove typography settings

--- src/components/rule/RuleDocumentation.tsx
+++ src/components/rule/RuleDocumentation.tsx
@@ -1,3 +1,45 @@
+import React from "react";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
 export default function RuleDocumentation() {
-  return <div>TODO Rule Documentation!</div>;
+  return (
+    <div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Examples</Typography>
+        </AccordionSummary>
+        <AccordionDetails></AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Test Coverage</Typography>
+        </AccordionSummary>
+        <AccordionDetails></AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Code</Typography>
+        </AccordionSummary>
+        <AccordionDetails></AccordionDetails>
+      </Accordion>
+    </div>
+  );
 }
--- src/themes/LightThemeOptions.ts
+++ src/themes/LightThemeOptions.ts
@@ -20,30 +20,10 @@ export const themeOptions: ThemeOptions = {
     },
   },
   typography: {
-    h1: {
-      fontWeight: 1000,
-    },
-    h2: {
-      fontWeight: 800,
-    },
-    h3: {
-      fontWeight: 600,
-    },
-    h4: {
-      fontWeight: 600,
-    },
-    h5: {
-      fontWeight: 700,
-    },
-    subtitle1: {
-      fontWeight: 600,
-    },
-    button: {
+     button: {
       textTransform: "none",
     },
     fontFamily: "Montserrat",
-    fontWeightMedium: 700,
-    fontWeightBold: 1000,
   },
   shape: {
     borderRadius: 4,
