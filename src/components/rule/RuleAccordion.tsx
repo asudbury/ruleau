a7990a36ea70da68e782f3deb2161a27c2a72523
Adrian Sudbury <sudburya@gmail.com>
2021-03-15 10:12:42 +0000

    Update to sample data

--- src/components/rule/RuleAccordion.tsx
+++ src/components/rule/RuleAccordion.tsx
@@ -37,7 +37,7 @@ export default function RuleAccordion({
       <Grid item>
         <Typography>{ruleName}</Typography>
       </Grid>
-      <Grid item>
+      <Grid item style={{ minWidth: 200 }}>
         <Typography>{ruleDescription}</Typography>
       </Grid>
       <Grid item>
